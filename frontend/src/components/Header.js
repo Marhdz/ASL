198e05c9d103c413c3a31035d684ca957a3a939b
Michelle Romero <marhdz0@gmail.com>
2019-03-11 23:03:19 -0600

    search bar incomplete

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import App from '../App';
+
 
 const Header = (props) => {
   return (
@@ -10,19 +12,19 @@ const Header = (props) => {
           {props.branding}
         </a>
         <div>
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a href="/" className="nav-link">
-                Home
-              </a>
-            </li>
-          </ul>
+              <form className="form-inline md-form form-sm active-pink active-pink-2 mt-2">
+                <i className="fas fa-search"></i>
+                <button onClick={this.handleClick}/>
+                // <input className="form-control form-control-sm ml-3 w-75" type="text" onClick={App.handleChange} placeholder="Search" aria-label="Search"></input>
+              </form>
         </div>
       </div>
     </nav>
   );
 };
-
+handleClick() {
+  console.log('Click happened');
+};
 Header.defaultProps = {
   branding: 'My App'
 };
